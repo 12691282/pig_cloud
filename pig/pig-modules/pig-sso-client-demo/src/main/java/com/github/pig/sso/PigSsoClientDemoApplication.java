aa87bfcd824302d7cf9861c97ba157f9e4a9b5b3
apple <12691282@qq.com>
2019-01-04 16:59:16 +0800

    update bootstrap yml config

--- pig/pig-modules/pig-sso-client-demo/src/main/java/com/github/pig/sso/PigSsoClientDemoApplication.java
+++ pig/pig-modules/pig-sso-client-demo/src/main/java/com/github/pig/sso/PigSsoClientDemoApplication.java
@@ -17,18 +17,16 @@
 
 package com.github.pig.sso;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
 
 /**
  * @author lengleng
@@ -41,21 +39,59 @@ public class PigSsoClientDemoApplication {
 
 
     public static void main(String[] args) {
-//        String url = "http://127.0.0.1:9999/auth/oauth/token";
-        String url = "http://192.168.1.114:55015/bus/refresh";
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
-        headers.setContentType(type);
-        headers.add("Accept", MediaType.APPLICATION_JSON.toString());
 
-        HttpEntity<String> formEntity = new HttpEntity<String>("", headers);
-        String result = restTemplate.postForObject(url, formEntity, String.class);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("User-Agent","Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36");
+        headers.add("Accept-Encoding", "gzip,deflate");
+        headers.add("Accept-Language", "zh-CN");
+        headers.add("Connection", "Keep-Alive");
+        String code = "lion:lion";
+        String auth = "Basic "+PigSsoClientDemoApplication.encode(code);
+        headers.add("Authorization", auth);
+        MultiValueMap<String, Object> paramMap = new LinkedMultiValueMap();
+        paramMap.add("username",  "admin");
+        paramMap.add("password",  "$2a$10$vg5QNHhCknAqevx9vM2s5esllJEzF/pa8VZXtFYHhhOhUcCw/GWyS");
+        paramMap.add("randomStr", "19201544884633838");
+        paramMap.add("code","1111");
+        paramMap.add("grant_type","password");
+        paramMap.add("scope","server");
+        String url =  "http://127.0.0.1:56513/bus/refresh";
+//        String url = "http://127.0.0.1:9999/auth/oauth/token";
+
+        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity(paramMap,headers);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> result = restTemplate.postForEntity(url, httpEntity, String.class);
+        System.out.println( result);
 
 //        SpringApplication.run(PigSsoClientDemoApplication.class, args);
-       System.out.println( result);
+
     }
 
 
+    /**
+     * 编码
+     *
+     * @param content
+     * @return
+     */
+    public static String encode(String content) {
+        return new sun.misc.BASE64Encoder().encode(content.getBytes());
+    }
 
+    /**
+     * 解码
+     *
+     * @param source
+     * @return
+     */
+    public static String decode(String source) {
+        try {
+            sun.misc.BASE64Decoder decoder = new sun.misc.BASE64Decoder();
+            return new String(decoder.decodeBuffer(source));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
